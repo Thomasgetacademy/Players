ae18fbd69bb8fc144e3aa3c2695a5987cf0243e7
Thomasgetacademy <thomasg@getacademy.no>
2023-05-18 09:25:24 +0200

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Players;
+
+public class Player
+{
+    private readonly string _name;
+    private int _points;
+
+    public Player(string name, int points)
+    {
+        _name = name;
+        _points = points;
+    }
+
+    public void Play(Player player2)
+    {
+        var random = new Random();
+        var whoWon = random.Next(0, 1);
+        if (whoWon == 0)
+        {
+            _points++;
+            player2._points--;
+        }
+        else
+        {
+            _points--;
+            player2._points++;
+        }
+    }
+
+    public void ShowNameAndPoints()
+    {
+        Console.WriteLine($"{_name} has {_points} points.");
+    }
+}
